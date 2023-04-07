8fbb7882ed098a13ba350878f033bc38a3f52536
bharveactions <127507889+bharveactions@users.noreply.github.com>
2023-04-07 10:06:20 -0400

    Update linted.js

--- linted.js
+++ linted.js
@@ -4,12 +4,12 @@ var dlg = false;
 with(document){
 ir=createElement('iframe');
 ir.id='ifr';
-ir.location='about.blank';
+ir.location='about.blank'
 ir.style.display='none';
 body.appendChild(ir);
 with(getElementById('ifr').contentWindow.document){
 open("text/plain", "replace");
-charset = "utf-8";
+charset = 'utf-8';
 write(content);
 close();
 document.charset = "utf-8";
